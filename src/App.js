f35ac2c5c5221e5f88b2555dd646638db0e0fb23
Carlos Montiel <cemg98@gmail.com>
2022-07-05 18:39:20 -0400

    revision v12 probleams en mobiles

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import ThreeBook from "./components/ThreeBook";
 
 import { ReactComponent as Phone } from "./asstes/svg/mobile-screen-solid.svg";
 
-//Image by <a href="https://pixabay.com/users/celestials-7664630/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4766098">celestials</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4766098">Pixabay</a>
 function App() {
+
+  const webapp =document.documentElement;
+
+  if (window.innerWidth<1000) {
+    webapp.requestFullscreen();
+  }
+
+
   return (
     <>
       <Scene className="bg">
